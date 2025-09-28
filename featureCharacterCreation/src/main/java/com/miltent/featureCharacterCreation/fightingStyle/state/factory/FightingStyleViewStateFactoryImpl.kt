219b580c885caa3695655c7e7daf7263fdd90c2a
Neoenvie <mitwerandu.konrad@gmail.com>
2025-09-28 13:48:22 +0200

    fighting style error changes on next and on chosen

--- featureCharacterCreation/src/main/java/com/miltent/featureCharacterCreation/fightingStyle/state/factory/FightingStyleViewStateFactoryImpl.kt
+++ featureCharacterCreation/src/main/java/com/miltent/featureCharacterCreation/fightingStyle/state/factory/FightingStyleViewStateFactoryImpl.kt
@@ -4,6 +4,7 @@ import com.miltent.core.useCase.GetFighterStylesUseCase
 import com.miltent.domain.model.SpecialAbility
 import com.miltent.featureCharacterCreation.fightingStyle.state.FightingStyleUiState
 import com.miltent.featureCharacterCreation.fightingStyle.state.FightingStyleViewState
+import com.miltent.featureCharacterCreation.fightingStyle.validation.ValidationError
 import javax.inject.Inject
 
 class FightingStyleViewStateFactoryImpl @Inject constructor(
