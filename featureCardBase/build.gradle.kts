247b51f61569ac3ebac01e989bb73180993293fa
Neoenvie <mitwerandu.konrad@gmail.com>
2025-10-28 20:13:30 +0100

    poprawki

--- featureCardBase/build.gradle.kts
+++ featureCardBase/build.gradle.kts
@@ -47,9 +47,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.ui.tooling)
 }
