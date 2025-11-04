f8653a1578b023f24e34d777e08f2e4282bb57b0
Neoenvie <mitwerandu.konrad@gmail.com>
2025-11-04 17:21:45 +0100

    DND-26 -CharacterDbToDomainMapper -> adding mapping for baseAttributes and temporaryAttributes -CharacterDomainToEntityMapper -> changing mapping so that new baseAttributes and temporaryAttributes are in use

--- database/src/main/java/com/miltent/database/mapper/dbToDomain/CharacterDbToDomainMapper.kt
+++ database/src/main/java/com/miltent/database/mapper/dbToDomain/CharacterDbToDomainMapper.kt
@@ -11,6 +11,7 @@ import com.miltent.domain.model.Character
 import com.miltent.domain.model.HealthPoints
 import com.miltent.domain.model.MovementSpeed
 import com.miltent.domain.model.Race
+import com.miltent.domain.model.StatisticType
 
 class CharacterDbToDomainMapper @Inject constructor(
     private val raceDbToDomainMapper: Mapper<RaceEntity, Race>,
@@ -26,8 +27,26 @@ class CharacterDbToDomainMapper @Inject constructor(
             characterClass = characterClass,
             race = raceDbToDomainMapper.map(value.race),
             level = characterClass.level,
-            baseAttributes = Attributes(),
-            temporaryAttributes = Attributes(0),
+            baseAttributes = Attributes(
+                mutableMapOf<StatisticType, Attribute>(
+                    StatisticType.STR to Attribute(value.baseStrength),
+                    StatisticType.DEX to Attribute(value.baseDexterity),
+                    StatisticType.CON to Attribute(value.baseConstitution),
+                    StatisticType.INT to Attribute(value.baseIntelligence),
+                    StatisticType.WIS to Attribute(value.baseWisdom),
+                    StatisticType.CHA to Attribute(value.baseCharisma)
+                )
+            ),
+            temporaryAttributes = Attributes(
+                mutableMapOf<StatisticType, Attribute>(
+                    StatisticType.STR to Attribute(value.temporaryStrModifier),
+                    StatisticType.DEX to Attribute(value.temporaryDexModifier),
+                    StatisticType.CON to Attribute(value.temporaryConModifier),
+                    StatisticType.INT to Attribute(value.temporaryIntModifier),
+                    StatisticType.WIS to Attribute(value.temporaryWisModifier),
+                    StatisticType.CHA to Attribute(value.temporaryChaModifier)
+                )
+            ),
             baseStrength = Attribute(value.baseStrength),
             baseDexterity = Attribute(value.baseDexterity),
             baseConstitution = Attribute(value.baseConstitution),
--- database/src/main/java/com/miltent/database/mapper/domainToDb/CharacterDomainToEntityMapper.kt
+++ database/src/main/java/com/miltent/database/mapper/domainToDb/CharacterDomainToEntityMapper.kt
@@ -7,6 +7,7 @@ import com.miltent.database.entities.character.RaceEntity
 import com.miltent.domain.model.Character
 import com.miltent.domain.model.CharacterClass
 import com.miltent.domain.model.Race
+import com.miltent.domain.model.StatisticType
 import javax.inject.Inject
 
 class CharacterDomainToEntityMapper @Inject constructor(
@@ -17,7 +18,19 @@ class CharacterDomainToEntityMapper @Inject constructor(
         name = value.name,
         characterClass = characterClassDomainToEntityMapper.map(value.characterClass),
         race = raceDomainToEntityMapper.map(value.race),
-        baseStrength = value.baseStrength.value,
+        baseStrength = value.baseAttributes.values[StatisticType.STR]!!.value,
+        baseDexterity = value.baseAttributes.values[StatisticType.DEX]!!.value,
+        baseConstitution = value.baseAttributes.values[StatisticType.CON]!!.value,
+        baseIntelligence = value.baseAttributes.values[StatisticType.INT]!!.value,
+        baseWisdom = value.baseAttributes.values[StatisticType.WIS]!!.value,
+        baseCharisma = value.baseAttributes.values[StatisticType.CHA]!!.value,
+        temporaryStrModifier = value.temporaryAttributes.values[StatisticType.STR]!!.value,
+        temporaryDexModifier = value.temporaryAttributes.values[StatisticType.DEX]!!.value,
+        temporaryConModifier = value.temporaryAttributes.values[StatisticType.CON]!!.value,
+        temporaryIntModifier = value.temporaryAttributes.values[StatisticType.INT]!!.value,
+        temporaryWisModifier = value.temporaryAttributes.values[StatisticType.WIS]!!.value,
+        temporaryChaModifier = value.temporaryAttributes.values[StatisticType.CHA]!!.value,
+        /*baseStrength = value.baseStrength.value,
         baseDexterity = value.baseDexterity.value,
         baseConstitution = value.baseConstitution.value,
         baseIntelligence = value.baseIntelligence.value,
@@ -28,7 +41,7 @@ class CharacterDomainToEntityMapper @Inject constructor(
         temporaryConModifier = value.temporaryConModifier.value,
         temporaryIntModifier = value.temporaryIntModifier.value,
         temporaryWisModifier = value.temporaryWisModifier.value,
-        temporaryChaModifier = value.temporaryChaModifier.value,
+        temporaryChaModifier = value.temporaryChaModifier.value,*/
         movementSpeed = value.movementSpeed.value,
         maxHealthPoints = value.healthPoints.max,
         currantHealthPoints = value.healthPoints.current,
