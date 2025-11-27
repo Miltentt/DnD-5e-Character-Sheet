1f79bcd2d9461b9d0207733b166615c93410855e
Neoenvie <mitwerandu.konrad@gmail.com>
2025-11-27 15:08:13 +0100

    DND-33

--- featureDashboard/src/main/java/com/miltent/featuredashboard/state/DashboardUiState.kt
+++ featureDashboard/src/main/java/com/miltent/featuredashboard/state/DashboardUiState.kt
@@ -2,5 +2,5 @@ package com.miltent.featuredashboard.state
 
 import com.miltent.core.ui.ViewState
 
-class DashboardUiState(val characterLongClickedId: String? = null): ViewState {
+data class DashboardUiState(val characterLongClickedId: String? = null): ViewState {
 }
