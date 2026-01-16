8e99bd9b639ca052d1301d074a7e83b35d0b815e
Neoenvie <mitwerandu.konrad@gmail.com>
2026-01-16 10:33:43 +0100

    DND-39 -text no_characters aligned to center

--- featureDashboard/src/main/java/com/miltent/featuredashboard/ui/DashboardScreen.kt
+++ featureDashboard/src/main/java/com/miltent/featuredashboard/ui/DashboardScreen.kt
@@ -15,12 +15,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -84,6 +81,7 @@ private fun DashboardScreen(
                             stringResource(ResR.string.no_characters),
                             color = Colors.primary,
                             fontSize = 22.sp,
+                            textAlign = TextAlign.Center,
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(Spacing.spacing16)
